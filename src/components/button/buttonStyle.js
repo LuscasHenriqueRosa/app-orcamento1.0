779a1316995309777199947ad9920e162b40107c
Lucas Henrique Rosa <luscasdeve@gmail.com>
2024-01-30 20:22:37 -0300

    Ajuste de teste no style

--- src/components/button/buttonStyle.js
+++ src/components/button/buttonStyle.js
@@ -2,15 +2,17 @@
 import styled from 'styled-components/native';
 
 const Container = styled.View`
-  flex: 1;
-  background-color: #f2f2f2;
+  background-color: #2b3;
   align-items: center;
   justify-content: center;
+  width:90%;
+  height:50px ;
+  border-radius:7px ;
 `;
 
 const TextStyled = styled.Text`
   font-size: 20px;
-  color: #333;
+  color: #fff;
 `;
 
 export default {
