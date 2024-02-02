b7c41d026cf95686ac9eed1e6c085c123dd61da9
Lucas Henrique Rosa <luscasdeve@gmail.com>
2024-02-01 21:10:43 -0300

    APP styled

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+ const Container = styled.View`
+  flex: 1;
+  background-color: #f2f;
+  align-items: center;
+  justify-content: center;
+`;
+
+ const TextStyle = styled.Text`
+  font-size: 20px;
+  color: #fff;
+`;
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola</Text>
+    <Container>
+      <TextStyle>Hola</TextStyle>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f2f2f2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f2f2f2',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
